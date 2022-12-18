da9c36888e14946df9f69f2c217c05587b0039ff
bahadirgezer <bahadirgezerbg@gmail.com>
2022-12-18 20:24:52 +0300

    requirements 2 and 3 finished. Both merging methods implemented.

--- projects/project2/src/main.py
+++ projects/project2/src/main.py
@@ -1,6 +1,10 @@
 from mpi4py import MPI
 import sys
+
+from mpi4py.MPI import Request
+
 from bigram.Bigram import Master
+
 
 # mpiexec -n 4 python main.py --input_file ../resources/sample_text.txt --merge_method MASTER --test_file ../resources/test.txt
 def main() -> None:
@@ -9,21 +13,65 @@ def main() -> None:
     comm: MPI.Intracomm = MPI.COMM_WORLD
     rank: int = comm.rank
     size = comm.size
+    freqs: dict = dict()
+    master: Master = Master(sys.argv, size - 1)
 
     if rank == 0:
         # the master will be rank 0, so we need to subtract 1 from the size
-        master: Master = Master(sys.argv, size-1)
+
         for worker in range(1, size):
-            comm.send(master.data[worker-1], dest=worker)
-        comm.barrier()
+            comm.send(master.data[worker - 1], dest=worker)
+        # comm.barrier()
+
+        # the master will be rank 0, so we need to subtract 1 from the size
+        if master.merge_method == "MASTER":
+            for worker in range(1, size):
+                calculated: dict = comm.recv(source=worker)
+                freqs = {k: freqs.get(k, 0) + calculated.get(k, 0) for k in set(freqs).union(calculated)}
+
+        elif master.merge_method == "WORKERS":
+            calculated: dict = comm.recv(source=size - 1)
+            freqs = {k: freqs.get(k, 0) + calculated.get(k, 0) for k in set(freqs).union(calculated)}
+
+        # print the largest 20 values in the dictionary with the key and value pair in a sorted order
+        print(sorted(freqs.items(), key=lambda x: x[1], reverse=True)[:20])
+
     else:
         # slaves
         work: list[str] = comm.recv(source=0)
         print(f"Rank: {rank}, Sentences: {len(work)}")
-        comm.barrier()
 
+        for sentence in work:  # unigrams
+            for word in sentence.split():
+                freqs[word] = freqs.get(word, 0) + 1
 
+        for sentence in work:  # bigrams
+            words = sentence.split()
+            for i in range(len(words) - 1):
+                bigram = words[i] + " " + words[i + 1]
+                freqs[bigram] = freqs.get(bigram, 0) + 1
 
+        if master.merge_method == "MASTER":
+            comm.send(freqs, dest=0)
+
+        elif master.merge_method == "WORKERS":
+            # receive data from previous worker
+            calculated: dict = dict()  # empty dict for the first worker
+            if rank != 1:
+                calculated: dict = comm.recv(source=rank - 1)
+                freqs = {k: freqs.get(k, 0) + calculated.get(k, 0) for k in set(freqs).union(calculated)}
+            dest_rank = rank + 1 if rank + 1 < size else 0
+            comm.send(freqs, dest=dest_rank)
+
+            # receive the data from the previous worker
+            # send the calculated frequencies to the next worker, if the next worker is the master, send it to the
+            # master each worker merges its own frequencies with the frequencies of the next worker this might cause
+            # deadlocks if the workers are not in the correct order e.g. worker 1 sends to worker 2, worker 2 sends
+            # to worker 3, worker 3 sends to worker 0 if the size is 4 if the workers are not in the correct order,
+            # the workers will deadlock send it in a non-blocking way so that the workers can continue to work
+
+        else:
+            raise ValueError("Invalid merge method.")
 
     return None
 
