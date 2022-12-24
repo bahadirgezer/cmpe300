689d820aeb1a2e2d3fde02ee7b1383f081154e65
BatuhanIlhan <mbatuhanilhan@gmail.com>
2022-12-24 23:02:42 +0300

    complete partition types

--- projects/project3/src/analysis/algorithm.py
+++ projects/project3/src/analysis/algorithm.py
@@ -28,6 +28,7 @@ class Algorithm:
 
     def run(self) -> None:  # run the algorithm
         self.quick_sort(0, len(self.data) - 1)
+        self.check_if_data_sorted()
 
     def quick_sort(self, low: int, high: int) -> None:
         """This is the quick sort algorithm. It calls the partition function and then recursively calls itself on the
@@ -42,7 +43,40 @@ class Algorithm:
         """This is the partition function for the classical deterministic algorithm. The pivot is chosen as the
         first element of the list. """
         pivot = self.data[low]
-        i = low + 1  # index of smaller element
+        return self.make_swaps(low, high, pivot)
+
+    def partition2(self, low: int, high: int) -> int:
+        """This is the partition function for the randomized algorithm. The pivot is chosen randomly. """
+        pivot_index = random.randint(low, high)
+        pivot = self.data[pivot_index]
+        self.data[pivot_index], self.data[low] = self.data[low], self.data[pivot_index]
+        return self.make_swaps(low, high, pivot)
+
+    def partition3(self, low: int, high: int) -> int:
+        """The randomized algorithm. The list is first randomly permuted and then the classical deterministic
+        algorithm is called where the pivot is chosen as the first element of the list. """
+
+        random.shuffle(self.data[low:high + 1])
+        pivot = self.data[low]
+        return self.make_swaps(low, high, pivot)
+
+    def partition4(self, low: int, high: int) -> int:
+        """This is the partition function for the deterministic algorithm. The pivot is chosen according to the
+        “median of three” rule. """
+        values = {self.data[low]: low, self.data[(high + low) // 2]: (high + low) // 2, self.data[high]: high}
+        values_list = [self.data[low], self.data[(high + low) // 2], self.data[high]]
+        values_list.sort()
+        median=values[values_list[1]]
+        pivot = self.data[median]
+        self.data[median], self.data[low] = self.data[low], self.data[median]
+        return self.make_swaps(low, high, pivot)
+
+    def reset(self, data: List[int]) -> None:
+        """This method is used to reset the data. """
+        self.data = data
+
+    def make_swaps(self, low: int, high: int, pivot: int) -> int:
+        i = low + 1
         for j in range(low + 1, high + 1):  # from low + 1 to high
             if self.data[j] < pivot:
                 self.data[i], self.data[j] = self.data[j], self.data[i]  # swap
@@ -50,22 +84,7 @@ class Algorithm:
         self.data[low], self.data[i - 1] = self.data[i - 1], self.data[low]  # swap pivot with last element
         return i - 1
 
-    def partition2(self, low: int, high: int) -> int:
-        """This is the partition function for the randomized algorithm. The pivot is chosen randomly. """
-        pivot = random.choice(self.data[low:high + 1])
-
-        return 0
-
-    def partition3(self, low: int, high: int) -> int:
-        """The randomized algorithm. The list is first randomly permuted and then the classical deterministic
-        algorithm is called where the pivot is chosen as the first element of the list. """
-        return 0
-
-    def partition4(self, low: int, high: int) -> int:
-        """This is the partition function for the deterministic algorithm. The pivot is chosen according to the
-        “median of three” rule. """
-        return 0
-
-    def reset(self, data: List[int]) -> None:
-        """This method is used to reset the data. """
-        self.data = data
+    def check_if_data_sorted(self):
+        for i in range(1, len(self.data)):
+            if self.data[i - 1] > self.data[i]:
+                raise ValueError("NOT SORTED")
